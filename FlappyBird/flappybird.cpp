7cc11663c622a525d30c01dd1f1a5b0ffac58d21
Alexander Dyachenka <alexdyachenka@gmail.com>
2020-05-12 23:00:04 +0300

    Creating Project

---
+++ FlappyBird/flappybird.cpp
@@ -0,0 +1,15 @@
+#include "flappybird.h"
+#include "ui_flappybird.h"
+
+FlappyBird::FlappyBird(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::FlappyBird)
+{
+    ui->setupUi(this);
+}
+
+FlappyBird::~FlappyBird()
+{
+    delete ui;
+}
+
